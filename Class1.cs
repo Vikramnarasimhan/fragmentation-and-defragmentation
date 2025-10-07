0f1e826b01ada64c6b46eb10861630d24c64f956
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-07 11:30:08 +0530

    getting inputs from the user effectively

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace readinguser {
+    class readinginput
+    {
+        static void Main(string[] args) {
+
+
+           Console.WriteLine("Hi enter some text and once you want to end writing just type END ");
+            string line;
+            string multilineinput = "";
+            while ((line = Console.ReadLine()) != "END") {
+                multilineinput +=line+ Environment.NewLine;
+            
+            }
+            Console.WriteLine("Your document contents");
+           Console.WriteLine($"{multilineinput}");
+        
+        }
+
+     }
+
+
+
+
+
+}
