ef24a65f30a5287836179858dbffe2b868620a36
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-07 12:41:21 +0530

    writing contents into text file

--- Class1.cs
+++ Class1.cs
@@ -16,17 +16,63 @@ namespace readinguser {
                 multilineinput +=line+ Environment.NewLine;
             
             }
-            Console.WriteLine("Your document contents");
-           Console.WriteLine($"{multilineinput}");
-            string filepath = "Userdocumentation.txt";
-            File.WriteAllText(filepath,multilineinput);
-            // Console.WriteLine("enter number of characters you want per line from 1-5");
-           // string characterextraction=Console.ReadLine();
 
-        
+             Console.WriteLine("Input how many characters at a time to split and create. Enter from 1-10");
+            int charlength = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Reading  {charlength}  characters at a time");
+            List<string> chunks = new List<string>();
+            for (int i = 0; i < multilineinput.Length; i += charlength)
+            {
+                int length = Math.Min(charlength, multilineinput.Length - i);
+                string chunk = multilineinput.Substring(i, length);
+              //  Console.WriteLine($"{chunk}");
+                chunks.Add( chunk );
+            }
+            for (int i = 0; i < chunks.Count; i++) {
+                string filename = $"chunk_{i}.txt";
+                File.WriteAllText( filename, chunks[i] );
+           //     Console.WriteLine($"CHUNKS{filename} stored successfully");
+            }
+            Console.WriteLine("ALL CHUNKS RESPECTIVE TEXT FILES CREATED SUCCESSFULLY");
+            string[] chunkfiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "chunk_*.txt");
+            Array.Sort(chunkfiles);
+            string reconstructed = "";
+            foreach(string file in chunkfiles)
+            {
+                
+               string content= File.ReadAllText(file);
+                reconstructed+= content;
+            }
+            Console.WriteLine("ok these are my contents:");
+            Console.WriteLine(reconstructed);
+            //List<string> chunks = new List<string>();
+
+            //for (int i = 0; i < multilineinput.Length; i += charlength)
+            //{
+            //  int length = Math.Min(charlength, multilineinput.Length - i);
+            //string chunk = multilineinput.Substring(i, length);
+            //chunks.Add(chunk);
+            //}
+            //string reconstructed = string.Join("", chunks);
+            //Console.WriteLine("Reconstructed text:");
+            //Console.WriteLine(reconstructed);
+
+
+
+
+            // Console.WriteLine("Your document contents");
+            //Console.WriteLine($"{multilineinput}");
+
+
+            // string filepath = "Userdocumentation.txt";
+            // File.WriteAllText(filepath,multilineinput);
+            // Console.WriteLine("enter number of characters you want per line from 1-5");
+            // string characterextraction=Console.ReadLine();
+
+
         }
 
-     }
+    }
 
 
 
